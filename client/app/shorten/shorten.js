b9cd6ea5e1b0fe9b6f837cd656e9d6785e821d58
Hack Reactor Students <accounts@hackreactor.com>
2015-06-12 22:36:20 -0700

    Minor changes

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -4,14 +4,16 @@ angular.module('shortly.shorten', [])
   // Your code here
   $scope.linkPath = "links";
   $scope.link = {};
+  $scope.data.links = [];
   $scope.addLink = function() {
     Links.saveLink($scope.link, function(resp) {
       // $location.path('#/');
+      $scope.data.links.push($scope.link);
     });
   };
 
   $scope.signOut = function(){
     Auth.signout();
-  }
+  };
 
 });
